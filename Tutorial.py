db712cd51ca241efbb61ebfed790d98dce618a35
davitkhanal <davit.khanal@8squarei.com>
2024-07-09 10:34:20 +0545

    string check

--- Tutorial.py
+++ Tutorial.py
@@ -1,20 +1,25 @@
-import datetime
-first_name=input("What is your first name ? ")
-last_name=input("What is your last name ? ")
-age = input("What is your age ? ")
-is_newPatient = input("New or Old ")
-
-## check whether new patient or old
-if is_newPatient.lower() == "new":
-    is_newPatient = True
-else :
-    is_newPatient = False
-
-## get Year of Birth
-YOB = datetime.datetime.now().year - int(age)
-
-if is_newPatient == True:
-    print("New Patient",first_name,last_name,"has been added. having Year of birth to be",YOB)
-else :
-    print("Already present")
+# import datetime
+# first_name=input("What is your first name ? ")
+# last_name=input("What is your last name ? ")
+# age = input("What is your age ? ")
+# is_newPatient = input("New or Old ")
+#
+# ## check whether new patient or old
+# if is_newPatient.lower() == "new":
+#     is_newPatient = True
+# else :
+#     is_newPatient = False
+#
+# ## get Year of Birth
+# YOB = datetime.datetime.now().year - int(age)
+#
+# if is_newPatient == True:
+#     print("New Patient",first_name,last_name,"has been added. having Year of birth to be",YOB)
+# else :
+#     print("Already present")
+#
+#
+####################
+course = 'python for beginner'
+print(course.upper())
 
